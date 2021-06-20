770b62ae0337a075af9464b85067843dbb997e69
Shyam Jos <shyamjosepp@gmail.com>
2021-06-20 16:00:13 +0530

    Added missing http response headers

--- http_server.rb
+++ http_server.rb
@@ -11,9 +11,15 @@ loop {
   puts "#{method} #{path} #{version}"
 
   if path == "/healthcheck"
-    client.write("OK")
+    client.write "HTTP/1.1 200\r\n" 
+    client.write "Content-Type: text/html\r\n"
+    client.write "\r\n"
+    client.write("OK\r\n")
   else
-    client.write("Well, hello there!")
+    client.write "HTTP/1.1 200\r\n" 
+    client.write "Content-Type: text/html\r\n"
+    client.write "\r\n"
+    client.write("Well, hello there!\r\n")
   end
 
   client.close
